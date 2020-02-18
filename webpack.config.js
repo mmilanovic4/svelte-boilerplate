4e45dc36226b9922a76be7b5ff15094cfe9d3e89
Miloš Milanović <mmilanovic016@gmail.com>
2020-02-18 16:11:26 +0100

    Update outdated packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
@@ -22,7 +22,8 @@ module.exports = env => {
 			mainFields: ['svelte', 'browser', 'module', 'main']
 		},
 		output: {
-			path: path.resolve(__dirname, 'dist'),
+			path: path.resolve(__dirname, 'dist', 'assets'),
+			publicPath: 'assets/',
 			filename: '[name].js',
 			chunkFilename: '[name].[id].js'
 		},
