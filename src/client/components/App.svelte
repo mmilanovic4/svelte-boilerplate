1fe9bbd33590507e1e782fe2ce7ce51758643a57
Miloš Milanović <mmilanovic016@gmail.com>
2020-04-28 23:41:42 +0200

    Change example component

--- src/client/components/App.svelte
+++ src/client/components/App.svelte
@@ -1,12 +1,46 @@
 <script>
-	import config from 'Client/config.json';
-	console.log(config);
+	let count = 0;
+
+	function handleClick() {
+		count += 1;
+	}
 </script>
 
 <style>
-	h1 {
-		color: #444;
+	:global(body) {
+		padding: 20px;
+	}
+
+	div {
+		display: grid;
+		grid-auto-flow: row;
+		grid-gap: 20px;
+		justify-content: flex-start;
+	}
+
+	button {
+		padding: 20px 50px;
+		background-color: #222;
+		border: 2px solid #222;
+		border-radius: 4px;
+		color: #eee;
+		outline: none;
+		transition-property: box-shadow;
+		transition-duration: .3s;
+		transition-timing-function: ease-in-out;
+	}
+
+	button:hover {
+		box-shadow: 4px 4px 2px #888;
+	}
+
+	button:active {
+		transform: translateY(2px);
 	}
 </style>
 
-<h1>Hello</h1>
+<div>
+	<h1>Hello</h1>
+	<p>Counter: {count}</p>
+	<button on:click={handleClick}>Click me</button>
+</div>
